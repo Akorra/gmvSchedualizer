bf87adb3adb60258aec4c63a498254a002d5005a
Akorra <neverfearphilishere@gmail.com>
2019-01-17 23:54:22 +0000

    dinamify

--- Schedualizer/schedualizer.py
+++ Schedualizer/schedualizer.py
@@ -4,37 +4,101 @@ import sys
 import pandas as pd
 import numpy as np
 from pandas.tseries.offsets import BDay
+from pathlib import Path
 
-if(len(sys.argv) != 5):
-    strt = input('Start date: ')
-    end = input('Start date: ')
-    dformat = input('first element[day/month]: ')
-    project = input('project: ')
-    activity= input('activity: ')
-else:
-    strt = sys.argv[0]
-    end = sys.argv[1]
-    dformat = sys.argv[2]
-    project = sys.argv[3]
-    activity= sys.argv[4]
 
-if(dformat=='month'):
-    d1 = pd.to_datetime(strt, format='%m/%d/%Y')
-    d2 = pd.to_datetime(end, format='%m/%d/%Y')
-else:
-    d1 = pd.to_datetime(strt, format='%d/%m/%Y')
-    d2 = pd.to_datetime(end, format='%d/%m/%Y')
+def set_format(ind='\t'):
+    dformat = input(ind + '[+] Just so we\'re clear, will you use american date format or the right one? [american/correct]: ')
+    while(True):
+        if(dformat=='american'):
+            format = '%m/%d/%Y'
+            break
+        elif(dformat=='correct'):
+            format = '%d/%m/%Y'
+            break
+        dformat = input(ind + '[+] Just try again... [american/correct]: ')
+    return format
 
-df = pd.DataFrame(pd.date_range(d1, d2), columns=['date'])
-df = df.set_index('date')
-df = df[df.index.dayofweek < 5]
-df['datef'] = df.index.strftime("%d/%m/%Y")
-df['hours'] = np.where(df.index.dayofweek==4, "6,25", "8,75")
-df['project'] = project
-df['activity'] = activity
+def hookie_from_file(format, ind='\t'):
+    hookie_l = []
+    res = ind + '[+] okay give me a filename: '
+    file_n = 'n'
+    while(True):
+        file_n = input(res)
+        if(file_n == 'n'):
+            break
+        hookie_p = Path("bin/hookie/" + file_n)
+        if(hookie_p.is_file()):
+            break
+        else:
+            res = ind + '\t[-] Try again?[filename/n]:'
+    if(file_n != 'n'):
+        hookie_f = open(hookie_p, 'r')
+        try:
+            lines = hookie_f.readlines()
+            for l in lines:
+                hookie_l.append(pd.to_datetime(l, format=format))
+        except:
+            print(ind + '\t[-] Bad Date format.')
+            hookie_l = 'er'
+    return hookie_l
 
-filename = input("Select a filename:")
+print('[+] Time to Schedualize your work!')
+filename = input('\t[+] Select a filename: ')
 if(filename == None or filename==""):
-    filename = 'hours'
-    
+    filename = 'time_sheet'
+
+format = set_format(ind='\t')
+
+hookie_l = []
+res = '\t[+] Dude I have this file for days to exclude, wanna use it?[y/n]: '
+while(input(res) == 'y'):
+    hookie_l = hookie_from_file(format, ind='\t')
+    if(hookie_l != 'er'):
+        break
+    res = '\t[-] Want to try again?'
+
+if(input('\t[+] Want to instead just feed me the days you want to exclude?[y/n]: ') == 'y'):
+    hookie_f = open("bin/hookie/hookie_" + filename + ".conf", 'w+')
+    print('\t\t... just press enter when you\'re done')
+    while(True):
+        hookie_date = input('\t\t[+] Date [' + format.replace('%', '') + ']: ')
+        if(hookie_date != ''):
+            try:
+                hookie_l.append(pd.to_datetime(hookie_date, format=format))
+                hookie_f.write(hookie_date)
+            except:
+                print('\t\t\t[-] Bad Date format.')
+        else:
+            break
+
+columns = ['date']
+print('\t[+] In order, tell me which fields you need on your timesheet[name/enter]:')
+print('\t\t[+] 0. date')
+i = 0
+while(True):
+    i += 1
+    field = input('\t\t[+] ' + str(i) + '. ')
+    if(field == ""):
+        break
+    columns.append(field)
+
+df = pd.DataFrame(columns=columns)
+while(True):
+    strt = input('\t\t[+] Start of activity [' + format.replace('%','') + ']: ')
+    end = input('\t\t[+] End date [' + format.replace('%','') + ']: ')
+    d1 = pd.to_datetime(strt, format=format)
+    d2 = pd.to_datetime(end, format=format)
+    tmp = pd.DataFrame(pd.date_range(d1, d2), columns=['timestamp'])
+    tmp = tmp[~tmp.timestamp.isin(hookie_l)]
+    tmp = tmp.set_index('timestamp')
+    tmp = tmp[(tmp.index.dayofweek < 5)]
+    #tmp['hours'] = np.where(tmp.index.dayofweek==4, "6,25", "8,75")
+    tmp['date'] = tmp.index.strftime("%d/%m/%Y")
+    #tmp['hours'] = np.where(tmp.index.dayofweek==4, "6,25", "8,75")
+    for col in columns[1:]:
+        tmp[col] = input('\t\t[+] ' + col + ':' )
+    pd.concat([df, tmp])
+    if(input('\t[+] Is that it?[y/n]') == 'y'):
+        break
 df.to_csv(filename + ".csv", sep=';', header=False, index=False)
